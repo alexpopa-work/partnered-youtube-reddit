ce3f0979050a5ee9d3bf051cde4433933492f328
Alex Popa <alexpopawork@gmail.com>
2025-07-20 12:04:56 +0200

    Add handle support for channel validation, script improvements

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import moment, { Moment } from "moment";
 import { formatNumber, formatYoutubeLink } from "./util";
 import { RedditChild, Comment, Verification } from "./types";
 import { logger } from "./logger";
+import fs, { stat } from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -31,6 +33,73 @@ const youtube = google.youtube({
   auth: process.env["GOOGLE_API_KEY"],
 });
 
+const approvedLinks: {
+  userId: string;
+  commentId: string;
+  channelLink: string;
+}[] = [];
+
+/**
+ * Writes the approved YouTube links to a JSON file
+ */
+const writeApprovedLinksToFile = () => {
+  const filePath = path.join(__dirname, "approved.json");
+  const data = {
+    lastChecked: new Date().toISOString(),
+    approvedLinks,
+  };
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  logger.info(`Approved links written to ${filePath}`);
+};
+
+/**
+ * Reads the approved YouTube links from the JSON file
+ */
+const readApprovedLinksFromFile = () => {
+  const filePath = path.join(__dirname, "approved.json");
+  if (!fs.existsSync(filePath)) {
+    return { lastChecked: null, approvedLinks: [] };
+  }
+  const data = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(data);
+};
+
+/**
+ * Updates the flair for the approved channels
+ * @param approvedLinks
+ */
+const updateFlairForApprovedLinks = async (
+  approvedLinks: { userId: string; commentId: string; channelLink: string }[]
+) => {
+  for (const link of approvedLinks) {
+    const comment: Comment = {
+      author: link.userId,
+      created_utc: parseInt(link.commentId),
+      youtubeLink: link.channelLink,
+      body: "",
+      created: 0,
+    };
+    const verification = await verifyChannel(comment, true);
+
+    if (verification) {
+      await setFlair(verification);
+    }
+  }
+};
+
+/**
+ * Removes the flair for a specified Reddit user
+ * @param username
+ */
+const removeFlair = async (username: string) => {
+  logger.info(`Removing flair for /u/${username}`);
+  await reddit.post(`${SUBREDDIT}/api/selectflair`, {
+    name: username,
+    text: "", // Empty text removes the flair
+    flair_template_id: null, // Null template ID removes the flair
+  });
+};
+
 /**
  * Returns list of top level comments from a Reddit Post (Article)
  * Filters comments that includes YouTube links and created within a date range
@@ -38,30 +107,32 @@ const youtube = google.youtube({
  * @param startDate
  */
 const getRedditComments = async (articleSource: string, startDate: Moment) => {
-  const youtubeLinkReg = /https:\/\/www.youtube.com[^\s\[\])(]*/;
+  const youtubeLinkReg = /https:\/\/(www\.)?youtube\.com[^\s\[\])(]*/;
   const redditArticle: any[] = await reddit.get(articleSource, { sort: "new" });
   const [, replies] = redditArticle;
-
-  const allComments = replies.data.children;
 
   const more = replies.data.children.find(
     (reply: any) => reply.kind === "more"
   );
 
-  if (more) {
-    const commentIds = more.data.children;
-
-    const extraComments: any = await reddit.get("/api/morechildren", {
-      api_type: "json",
-      link_id: `t3_${ARTICLE}`,
-      children: commentIds.join(","),
-      limit_children: true,
-    });
-
-    allComments.push(...extraComments.json.data.things);
-  } else {
+  if (!more) {
     logger.info("No more comments to load.");
+    return [];
   }
+
+  const commentIds = more.data.children;
+
+  const extraComments: any = await reddit.get("/api/morechildren", {
+    api_type: "json",
+    link_id: `t3_${ARTICLE}`, // Post ID (t3_xxxx)
+    children: commentIds.join(","), // Comma-separated list of comment IDs
+    limit_children: true,
+  });
+  //logger.info(`Extra comments: ${JSON.stringify(extraComments)}`);
+  const allComments = [
+    ...replies.data.children,
+    ...extraComments.json.data.things,
+  ];
 
   const comments: Comment[] = allComments
     .filter((child: RedditChild) => child.data.author !== "[deleted]")
@@ -97,16 +168,24 @@ const getRedditComments = async (articleSource: string, startDate: Moment) => {
  * @param comment
  */
 const verifyChannel = async (
-  comment: Comment
+  comment: Comment,
+  skipDescriptionCheck = false
 ): Promise<Verification | undefined> => {
   const usernameReg = new RegExp(`u/${comment.author}`, "i");
   const youtubeChannelListParams: youtube_v3.Params$Resource$Channels$List = {
     part: ["snippet,statistics"],
   };
+
   if (!comment.youtubeLink) {
     return;
   }
-  const formattedYoutubeLink = formatYoutubeLink(comment.youtubeLink);
+
+  const formattedYoutubeLink = formatYoutubeLink(
+    comment.youtubeLink
+      .replace("https://youtube.com", "https://www.youtube.com")
+      .replace("https://m.youtube.com", "https://www.youtube.com")
+  ).split("?")[0];
+
   const [, slug] = formattedYoutubeLink.split("https://www.youtube.com/");
   if (!slug) {
     logger.warn(`Invalid YouTube link: ${formattedYoutubeLink}`);
@@ -115,12 +194,22 @@ const verifyChannel = async (
 
   logger.info(`Verifying /u/${comment.author} at ${formattedYoutubeLink}`);
 
+  let channelId;
+
   if (slug.includes("user/")) {
     youtubeChannelListParams.forUsername = slug.split("user/")[1];
   } else if (slug.includes("channel/")) {
-    youtubeChannelListParams.id = [slug.split("channel/")[1]];
+    channelId = slug.split("channel/")[1];
+  } else if (slug.startsWith("@")) {
+    const handle = slug.replace(/^@/, "");
+    // @ts-ignore
+    youtubeChannelListParams.forHandle = handle;
   } else {
     youtubeChannelListParams.forUsername = slug;
+  }
+
+  if (channelId) {
+    youtubeChannelListParams.id = [channelId];
   }
 
   const { data: channel } = await youtube.channels.list(
@@ -131,8 +220,13 @@ const verifyChannel = async (
     const { snippet, statistics } = channel.items[0];
 
     if (snippet?.description && statistics) {
-      if (usernameReg.test(snippet.description)) {
+      if (usernameReg.test(snippet.description) || skipDescriptionCheck) {
         logger.info(`/u/${comment.author} is verified`);
+        approvedLinks.push({
+          userId: comment.author,
+          commentId: comment.created_utc.toString(),
+          channelLink: formattedYoutubeLink,
+        });
         return {
           reddit: {
             comment,
@@ -188,7 +282,18 @@ const setFlair = async (verification: Verification) => {
 
 async function main() {
   logger.info("Starting script");
-  const startDate = moment.utc().subtract(PREVIOUS_DAYS, "days");
+
+  // Read the approved links from the JSON file
+  const { lastChecked, approvedLinks: existingApprovedLinks } =
+    readApprovedLinksFromFile();
+  approvedLinks.push(...existingApprovedLinks);
+
+  // Update flair for the approved links
+  await updateFlairForApprovedLinks(existingApprovedLinks);
+
+  const startDate = lastChecked
+    ? moment.utc(lastChecked)
+    : moment.utc().subtract(PREVIOUS_DAYS, "days");
 
   logger.info(
     `Retrieving comments from https://www.reddit.com${SUBREDDIT}/comments/${ARTICLE}`
@@ -212,6 +317,7 @@ async function main() {
     }
   }
 
+  //////// writeApprovedLinksToFile();
   logger.info("Script complete");
 }
 
